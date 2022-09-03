dab860bfa59aa60280551cea3bc5561991846268
Vignesh Ayyamani <automationbytes@gmail.com>
2022-09-03 22:05:50 +0530

    Added Day1

---
+++ JavaPrograms/LogicalOperator.java
@@ -0,0 +1,33 @@
+package JavaPrograms;
+
+public class LogicalOperator {
+public static void main(String[] args) {
+	int a = 10;
+	int b = 5;
+	int c = 15;
+	
+	System.out.println(a<b && a<c);
+	/*
+	 * true and true = true
+	 * true and false = false
+	 * false and true = false
+	 * false and false = false
+	 *  * 
+	 */
+	
+	System.out.println(a<b || a<c);
+	/*
+	 * true or true = true
+	 * true or false = true
+	 * false or true = true
+	 * false or false = false
+	 * 	 * 
+	 * 
+	 */
+
+	System.out.println(!(a<b || a<c));
+	
+	
+}
+
+}
---
+++ JavaPrograms/Typecasting.java
@@ -0,0 +1,12 @@
+package JavaPrograms;
+
+public class Typecasting {
+	public static void main(String[] args) {
+
+		//byte b = 10;
+		int c = 130;
+		byte b = (byte) c;//127,-128,-127,-126,-125....
+		
+		System.out.println(b);
+	}
+}
